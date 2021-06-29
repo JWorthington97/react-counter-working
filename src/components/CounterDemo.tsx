417aaad0c28c513010f5d3234da4ec3fcdd66955
JWorthington97 <jb.worthington@live.co.uk>
2021-06-29 12:01:28 +0100

    Add extra counter buttons

--- src/components/CounterDemo.tsx
+++ src/components/CounterDemo.tsx
@@ -9,16 +9,20 @@ function CounterDemo(): JSX.Element {
   };
 
   const handleReset = () => {
-    // TODO reset counter to 0 here
+    queueRerenderWithNewCounterValue(0);
   }
 
   return (
     <>
       <h1>Counter Demo</h1>
       <p>Current value: {counterValueFromCurrentRender}</p>
+      <button onClick={() => queueRerenderWithNewCounterValue(previousValue => previousValue - 5)}>- 5</button>
+      <button onClick={() => queueRerenderWithNewCounterValue(previousValue => previousValue - 1)}>- 1</button>
       <button onClick={handleAddOne}>+1</button>
+      <button onClick={() => queueRerenderWithNewCounterValue(previousValue => previousValue + 5)}>+ 5</button>
       <hr />
       <button onClick={handleReset}>Reset counter</button>
+
     </>
   );
 }
